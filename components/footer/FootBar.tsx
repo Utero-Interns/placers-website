117ebd38071e7bed81cf4192796647847c5483ce
febriantimayori <febriantimayori@gmail.com>
2025-10-03 17:24:11 +0700

    fix: enhance layout and improve accessibility in FootBar component

--- components/footer/FootBar.tsx
+++ components/footer/FootBar.tsx
@@ -3,54 +3,61 @@ import SocialIconLink from "./SocialIconLink";
 import FootLinkTitle from "./FootLinkTitle";
 import FootLink from "./FootLink";
 
-
 export default function FootBar() {
     return (
-
         <footer className="flex flex-col items-center bg-[var(--color-black2)] px-6 py-10 2xl:py-16">
-
             <div className="flex w-full flex-col gap-12 mb-10 lg:flex-row lg:items-start lg:justify-between lg:gap-8 2xl:w-11/12 2xl:items-center 2xl:mb-11">
 
-                <img 
-                    src="/logo-placers-white.png" 
-                    alt="Placers Logo" 
-                    className="h-24 w-24 self-center lg:h-32 lg:w-32 lg:self-start 2xl:h-48 2xl:w-48 2xl:m-16"
+                {/* Logo hanya tampil di desktop */}
+                <img
+                    src="/logo-placers-white.png"
+                    alt="Placers Logo"
+                    className="hidden lg:block h-24 w-24 mx-auto lg:mx-0 lg:my-auto lg:h-32 lg:w-32 2xl:h-48 2xl:w-48"
                 />
 
-                <div className="flex flex-col self-start space-y-[18px]">
+                {/* Site Map */}
+                <div className="flex flex-col self-start space-y-4">
                     <FootLinkTitle text="SITE MAP" />
-                    
                     <FootLink href="/populer" text="Populer" />
                     <FootLink href="/promo" text="Promo" />
                     <FootLink href="/blog" text="Blog" />
                 </div>
 
-                <div className="flex flex-col self-start space-y-[18px]">
+                {/* Social Media */}
+                <div className="flex flex-col self-start space-y-4">
                     <FootLinkTitle text="FOLLOW US" />
-
-                    <div className="flex space-x-8 2xl:space-x-10">
-                        <SocialIconLink href="https://www.facebook.com" Icon={Facebook} />
-                        <SocialIconLink href="https://www.twitter.com" Icon={Twitter} />
-                        <SocialIconLink href="https://www.instagram.com" Icon={Instagram} />
-                        <SocialIconLink href="https://www.youtube.com" Icon={Youtube} />
+                    <div className="flex space-x-6">
+                        <SocialIconLink href="https://www.facebook.com/uteroadvertisingindonesia" Icon={Facebook} />
+                        <SocialIconLink href="https://x.com/utero_indonesia" Icon={Twitter} />
+                        <SocialIconLink href="https://instagram.com/uteroindonesia" Icon={Instagram} />
+                        <SocialIconLink href="https://www.youtube.com/channel/UCkdJC5Tw0bk0xK9sUR80xnA" Icon={Youtube} />
                     </div>
                 </div>
 
-                <div className="flex w-full flex-col self-start space-y-[18px] lg:w-auto 2xl:w-[519px]">
-                    <FootLinkTitle text="CONTACT US" />
-
+                {/* Contact */}
+                <div className="flex w-full flex-col self-start space-y-3 lg:w-[380px]">
+                    <FootLinkTitle text="CONTACT" />
                     <FootLink href="https://uteroindonesia.com/" text="PT. Utero Kreatif Indonesia" />
-                    <FootLink href="https://maps.app.goo.gl/rSkKi7UfVtdWgyD99" text="Jalan Bantaran 1 No. 25 Tulusrejo, Kec. Lowokwaru Malang - Jawa Timur, Indonesia | Postal Code  65141" />
-                    <FootLink href="" text="+6281 999 900 900 (Wahyu)"/>
-                    <FootLink href="" text="+6281 7388 616 (Utero)"/>
-                    <FootLink href="mailto:marketingutero@gmail.com" text="marketingutero@gmail.com"/>
+                    <FootLink
+                        href="https://maps.app.goo.gl/rSkKi7UfVtdWgyD99"
+                        text="Jalan Bantaran I No. 25, Tulusrejo, Kec. Lowokwaru, Malang - Jawa Timur, Indonesia | Postal Code 65141"
+                    />
+                    <FootLink href="" text="+6281 999 900 900 (Wahyu)" />
+                    <FootLink href="" text="+6281 7388 616 (Utero)" />
+                    <FootLink href="mailto:marketingutero@gmail.com" text="marketingutero@gmail.com" />
                 </div>
-
             </div>
 
-            <div className="flex w-11/12 flex-col items-center border-t border-gray-700 pt-6 2xl:items-end">
-                {/* Font size is now responsive, starting smaller (text-sm) and scaling up to the original size (2xl:text-[20px]). */}
-                <p className="text-center text-sm lg:text-base 2xl:text-[20px] 2xl:text-right">
+            {/* Bottom Section */}
+            <div className="flex w-11/12 items-center border-t border-gray-700 pt-6 justify-between">
+                {/* Logo hanya tampil di mobile */}
+                <img
+                    src="/logo-placers-white.png"
+                    alt="Placers Logo"
+                    className="h-8 w-8 lg:hidden"
+                />
+
+                <p className="text-white text-xs lg:text-sm text-right w-full">
                     © 2025 Placers x TCW. All rights reserved
                 </p>
             </div>
