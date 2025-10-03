fc6e56fd0b1a5418fd9966c44ed2fb720aa2a85f
febriantimayori <febriantimayori@gmail.com>
2025-10-03 17:23:46 +0700

    fix: update text size in FootLink component for improved readability

--- components/footer/FootLink.tsx
+++ components/footer/FootLink.tsx
@@ -6,8 +6,8 @@ type FootLinkProps = {
 export default function FootLink({ href, text }: FootLinkProps) {
     return (
         <a 
-            className="text-base leading-relaxed hover:text-[var(--color-primary)] transition-colors duration-200 
-                       lg:text-lg 
+            className="text-sm text-white leading-relaxed hover:text-[var(--color-primary)] transition-colors duration-200 
+                       lg:text-sm 
                        2xl:text-[20px]" 
             href={href}
         >
